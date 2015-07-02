3ae8f974c3cfc5cc6f5c32c4aa32eb5c8d930f58
Arvid Kongstad <arvid@getnarrative.com>
2015-07-02 20:28:24 +0200

    Add progress bar view (currently with dummy label).

---
+++ Genlabbet/Views/ProgressBarView.swift
@@ -0,0 +1,58 @@
+import UIKit
+import Cartography
+
+class ProgressBarView: UIView {
+    
+    // MARK: - NSObject
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        
+        self.addSubview(self.dividerLine)
+        self.addSubview(self.dummyLabel)
+    }
+    
+    // MARK: - Subviews
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layout(self.dividerLine, self.dummyLabel) { divider, label in
+            divider.width   == divider.superview!.width
+            divider.height  == 0.5
+            divider.top     == divider.superview!.top
+            divider.centerX == divider.superview!.centerX
+            
+            label.width   == label.superview!.width
+            label.height  == 30.0
+            label.centerX == label.superview!.centerX
+            label.centerY == label.superview!.centerY
+        }
+    }
+    
+    private lazy var dummyLabel: UILabel = {
+        let dummyLabel = UILabel(frame: CGRectZero)
+        dummyLabel.font = UIFont(name: "HelveticaNeue", size: 15.0)
+        dummyLabel.textColor = UIColor.blackColor()
+        dummyLabel.textAlignment = .Center
+        dummyLabel.text = "Progress Bar Dummy Label"
+        dummyLabel.numberOfLines = 1
+        
+        return dummyLabel
+    }()
+    
+    private lazy var dividerLine: UIView = {
+        let dividerLine = UIView(frame: CGRectZero)
+        dividerLine.backgroundColor = UIColor(white: 0.5, alpha: 1.0)
+        
+        return dividerLine
+    }()
+}
