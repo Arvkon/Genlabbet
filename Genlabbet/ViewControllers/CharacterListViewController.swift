805db1dccd9a6d45cd4b3e18db28ffc7e82b8551
Arvid Kongstad <arvid@getnarrative.com>
2015-07-11 09:41:26 +0200

    Add and popuplate character list collection view.

--- Genlabbet/ViewControllers/CharacterListViewController.swift
+++ Genlabbet/ViewControllers/CharacterListViewController.swift
@@ -1,8 +1,80 @@
 import UIKit
 
-class CharacterListViewController: UIViewController {
+class CharacterListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Rollpersoner"
+        
+        self.view.addSubview(self.characterCollectionView)
+    }
+    
+    // MARK: - Views
+    
+    private lazy var characterCollectionView: UICollectionView = {
+        let verticalLayout = UICollectionViewFlowLayout()
+        verticalLayout.scrollDirection = .Vertical
+        verticalLayout.minimumLineSpacing = 2.0 // Vertical spacing
+        verticalLayout.minimumInteritemSpacing = 0.0 // Horizontal spacing
+        
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: verticalLayout)
+        collectionView.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.registerClass(
+            CharacterCollectionViewCell.self,
+            forCellWithReuseIdentifier: "CharacterCollectionViewCell"
+        )
+        
+        return collectionView
+    }()
+    
+    // MARK: - Methods
+    
+    func beginCharacterCreation() {
+        let animalViewController = AnimalViewController(character: Character())
+        let navigationController = UINavigationController(rootViewController: animalViewController)
+        self.presentViewController(navigationController, animated: true, completion: nil)
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    private let numberOfItemsInList = 5
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfItemsInList
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CharacterCollectionViewCell", forIndexPath: indexPath) as! CharacterCollectionViewCell
+        
+        if indexPath.row == self.numberOfItemsInList - 1 {
+            cell.setLabelText("Skapa ny rollperson")
+            cell.cellPressAction = { () -> Void in
+                self.beginCharacterCreation()
+            }
+        } else {
+            cell.setLabelText("Rollperson \(indexPath.row + 1)")
+            cell.cellPressAction = { () -> Void in
+                println("Did Press Character Cell at index \(indexPath.row)!")
+            }
+        }
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.size.width, height: 44.0)
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        collectionView.deselectItemAtIndexPath(indexPath, animated: true)
+        if let selectedCell = collectionView.cellForItemAtIndexPath(indexPath) as? CharacterCollectionViewCell {
+            selectedCell.performCellPressAction()
+        }
     }
 }
