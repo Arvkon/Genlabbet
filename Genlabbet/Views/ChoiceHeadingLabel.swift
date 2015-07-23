6af319ed141fb151328b4aaa58c18fce47a84237
Arvid Kongstad <arvid@getnarrative.com>
2015-07-23 23:55:57 +0200

    Add ChoiceHeadingLabel.

---
+++ Genlabbet/Views/ChoiceHeadingLabel.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ChoiceHeadingLabel: UILabel {
+    
+    // MARK: - Initialization
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(text labelText: String) {
+        super.init(frame: CGRectZero)
+        
+        self.font = UIFont(name: "Colfax-Regular", size: 22.0)
+        self.text = labelText
+    }
+}
